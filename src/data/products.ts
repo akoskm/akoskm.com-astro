7ed729be69bdf936984e86f1aa1b4a20af327e5e
Akos Komuves <akoskm@gmail.com>
2024-12-17 12:42:21 +0100

    add books section

---
+++ src/data/products.ts
@@ -0,0 +1,71 @@
+export default {
+  "rtl-crash-course": {
+    title: "React Testing Library Crash Course",
+    description:
+      "React Testing Library Crash Course: Learn to Test React Components takes you on a hands-on, practical journey to mastering component testing in React. From setting up your testing environment to tackling advanced scenarios, every chapter is written in a bite-sized, actionable style.",
+    links: [
+      {
+        href: "https://a.co/d/dyQ91Fw",
+        text: "Amazon",
+      },
+    ],
+    image: "/assets/covers/rtl-crash-course.jpg",
+  },
+  pwa: {
+    title: "Building Cloud-Based PWAs with Supabase, React & TypeScript",
+    description:
+      "Learn how to build modern PWAs backed by cloud services like Supabase, using React and TypeScript. The book covers PWA development fundamentals and deploying a cloud-based backend.",
+    links: [
+      {
+        href: "https://a.co/d/f2XsiSG",
+        text: "Amazon",
+      },
+      {
+        href: "https://akoskm.gumroad.com/l/supabase",
+        text: "Gumroad",
+      },
+    ],
+    image: "/assets/covers/supabase.jpg",
+  },
+  openAi: {
+    title: "Generative AI with React JS: Build and Deploy Powerful AI Apps",
+    description:
+      "Learn how to leverage the OpenAI API in React to create advanced generative AI applications. Throughout the book, you'll cover topics like generating text, speech post-processing, building a social media companion app, and deploying the final application.",
+    links: [
+      {
+        href: "https://a.co/d/9jnHQGe",
+        text: "Amazon",
+      },
+      {
+        href: "https://akoskm.gumroad.com/l/openai",
+        text: "Gumroad",
+      },
+    ],
+    image: "/assets/covers/openai.jpg",
+  },
+  portfolio: {
+    title: "How to Build Better Portfolio Projects - as a web developer",
+    description:
+      "Your career as a web developer starts here! Learn to make a portfolio project that will get you hired. 👨‍💻 This guide contains everything you need to know to build an eye-catching developer portfolio.\
+",
+    links: [
+      {
+        href: "https://akoskm.gumroad.com/l/how-to-build-better-portfolio-projects",
+        text: "Gumroad",
+      },
+    ],
+    image: "/assets/covers/portfolios.jpg",
+  },
+  hooks: {
+    title: "Custom React Hooks Crash Course: Learn Hooks Through 6 Apps",
+    description:
+      "Learn how to create custom React hooks to manage state, handle side effects, and make API calls. Gain a type-safe approach by incorporating custom React hooks in TypeScript to build scalable and maintainable front-end applications.",
+    links: [
+      {
+        href: "https://www.udemy.com/course/custom-react-hooks-crash-course",
+        text: "Udemy",
+      },
+    ],
+    image: "/assets/covers/reach-hooks.png",
+  },
+};
