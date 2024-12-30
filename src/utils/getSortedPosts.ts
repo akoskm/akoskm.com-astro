2601322f78e8002dc5173bef2bb6e55f7fd213b2
Akos Komuves <akoskm@gmail.com>
2024-12-30 10:20:29 +0100

    don't sort by updated at

--- src/utils/getSortedPosts.ts
+++ src/utils/getSortedPosts.ts
@@ -2,13 +2,7 @@ import postFilter from "./postFilter";
 import type { Post } from "lib/schema";
 
 const getSortedPosts = (posts: Post[]) => {
-  return posts
-    .filter(postFilter)
-    .sort(
-      (a, b) =>
-        Math.floor(new Date(b.updatedAt ?? b.publishedAt).getTime() / 1000) -
-        Math.floor(new Date(a.updatedAt ?? a.publishedAt).getTime() / 1000)
-    );
+  return posts.filter(postFilter);
 };
 
 export default getSortedPosts;
